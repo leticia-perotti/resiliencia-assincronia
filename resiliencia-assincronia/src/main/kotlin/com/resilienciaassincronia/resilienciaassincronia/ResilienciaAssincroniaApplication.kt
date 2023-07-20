3afdbac68716a75a3482757455c2d96e2f159502
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-07-20 10:00:22 -0300

    Initial commit

---
+++ resiliencia-assincronia/src/main/kotlin/com/resilienciaassincronia/resilienciaassincronia/ResilienciaAssincroniaApplication.kt
@@ -0,0 +1,11 @@
+package com.resilienciaassincronia.resilienciaassincronia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResilienciaAssincroniaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResilienciaAssincroniaApplication>(*args)
+}
